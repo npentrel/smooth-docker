5272084dd7f9413e801b8500f4114c04c5cbb422
Naomi Pentrel <npentrel@gmail.com>
2018-06-12 15:13:25 -0700

    Make changes easy

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__, static_url_path='')
 
+## Feel free to change this!
+pokemon = {
+    'pkdx_id': 1,
+    'name': 'Bulbasaur',
+    'art_url': 'http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+}
+
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("index.html", pokemon=pokemon)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=9000)
