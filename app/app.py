9ff69174935173bf618ac164a9171923dc9067d8
Naomi Pentrel <npentrel@gmail.com>
2018-06-19 13:18:18 -0700

    Change to smooth docker

--- app/app.py
+++ app/app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='')
 
 ## Feel free to change this!
-pokemon = {
+smoothie = {
     'pkdx_id': 1,
-    'name': 'Bulbasaur',
-    'art_url': 'http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+    'name': 'Mixed-Berry',
+    'img': 'https://www.maxpixel.net/static/photo/2x/Glass-Healthy-Smoothie-Milkshake-Mixed-Berry-2523597.jpg'
 }
 
 @app.route('/')
 def index():
-    return render_template("index.html", pokemon=pokemon)
+    return render_template("index.html", smoothie=smoothie)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
